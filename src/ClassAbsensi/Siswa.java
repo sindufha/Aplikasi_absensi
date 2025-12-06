6ec50e175ec12d791e901af2e7b2fd5f99b3c7b5
stnvtriana <sitinovitriana03@gmail.com>
2025-12-06 20:14:02 +0700

    get set status

--- src/ClassAbsensi/Siswa.java
+++ src/ClassAbsensi/Siswa.java
@@ -7,10 +7,13 @@ public class Siswa {
     private int idKelas;
     private String jenisKelamin;
     private String qrCode;
-
+    private String namaKelas; // TAMBAHAN - dari join dengan tabel kelas
+    private String status;
+    // Constructor kosong
     public Siswa() {
     }
-
+    
+    // Constructor lengkap dengan ID
     public Siswa(int idSiswa, String nis, String namaSiswa, int idKelas, String jenisKelamin, String qrCode) {
         this.idSiswa = idSiswa;
         this.nis = nis;
@@ -19,7 +22,8 @@ public class Siswa {
         this.jenisKelamin = jenisKelamin;
         this.qrCode = qrCode;
     }
-
+    
+    // Constructor tanpa ID (untuk insert)
     public Siswa(String nis, String namaSiswa, int idKelas, String jenisKelamin, String qrCode) {
         this.nis = nis;
         this.namaSiswa = namaSiswa;
@@ -27,55 +31,72 @@ public class Siswa {
         this.jenisKelamin = jenisKelamin;
         this.qrCode = qrCode;
     }
-
+    
+    // Getters dan Setters
     public int getIdSiswa() {
         return idSiswa;
     }
-
+    
     public void setIdSiswa(int idSiswa) {
         this.idSiswa = idSiswa;
     }
-
+    
     public String getNis() {
         return nis;
     }
-
+    
     public void setNis(String nis) {
         this.nis = nis;
     }
-
+    
     public String getNamaSiswa() {
         return namaSiswa;
     }
-
+    
     public void setNamaSiswa(String namaSiswa) {
         this.namaSiswa = namaSiswa;
     }
-
+    
     public int getIdKelas() {
         return idKelas;
     }
-
+    
     public void setIdKelas(int idKelas) {
         this.idKelas = idKelas;
     }
-
+    
     public String getJenisKelamin() {
         return jenisKelamin;
     }
-
+    
     public void setJenisKelamin(String jenisKelamin) {
         this.jenisKelamin = jenisKelamin;
     }
-
+    
     public String getQrCode() {
         return qrCode;
     }
-
+    
     public void setQrCode(String qrCode) {
         this.qrCode = qrCode;
     }
+    
+    // GETTER SETTER TAMBAHAN untuk namaKelas (dari JOIN)
+    public String getNamaKelas() {
+        return namaKelas;
+    }
+    
+    public void setNamaKelas(String namaKelas) {
+        this.namaKelas = namaKelas;
+    }
+    
+    public String getStatus() {
+    return status;
+}
 
+public void setStatus(String status) {
+    this.status = status;
+}
     @Override
     public String toString() {
         return "Siswa{" +
@@ -85,6 +106,7 @@ public class Siswa {
                 ", idKelas=" + idKelas +
                 ", jenisKelamin='" + jenisKelamin + '\'' +
                 ", qrCode='" + qrCode + '\'' +
+                ", namaKelas='" + namaKelas + '\'' +
                 '}';
     }
 }
