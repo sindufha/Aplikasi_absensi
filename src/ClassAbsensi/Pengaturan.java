e321a006fdc1c18c7d39ec6890d0a702c710b49d
sindufha <sindufha215@gmail.com>
2025-12-12 15:10:42 +0700

    edit

--- src/ClassAbsensi/Pengaturan.java
+++ src/ClassAbsensi/Pengaturan.java
@@ -1,13 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ClassAbsensi;
 
-/**
- *
- * @author MyBook Hype AMD
- */
 public class Pengaturan {
+    private int id;
+    private String namaSekolah;
+    private String alamatSekolah;
+    private String tahunAjaranAktif;
+    private String kepalaSekolah;
     
+    // Constructor kosong
+    public Pengaturan() {
+    }
+    
+    // Constructor lengkap
+    public Pengaturan(int id, String namaSekolah, String alamatSekolah, 
+                      String tahunAjaranAktif, String kepalaSekolah) {
+        this.id = id;
+        this.namaSekolah = namaSekolah;
+        this.alamatSekolah = alamatSekolah;
+        this.tahunAjaranAktif = tahunAjaranAktif;
+        this.kepalaSekolah = kepalaSekolah;
+    }
+    
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNamaSekolah() {
+        return namaSekolah;
+    }
+    
+    public void setNamaSekolah(String namaSekolah) {
+        this.namaSekolah = namaSekolah;
+    }
+    
+    public String getAlamatSekolah() {
+        return alamatSekolah;
+    }
+    
+    public void setAlamatSekolah(String alamatSekolah) {
+        this.alamatSekolah = alamatSekolah;
+    }
+    
+    public String getTahunAjaranAktif() {
+        return tahunAjaranAktif;
+    }
+    
+    public void setTahunAjaranAktif(String tahunAjaranAktif) {
+        this.tahunAjaranAktif = tahunAjaranAktif;
+    }
+    
+    public String getKepalaSekolah() {
+        return kepalaSekolah;
+    }
+    
+    public void setKepalaSekolah(String kepalaSekolah) {
+        this.kepalaSekolah = kepalaSekolah;
+    }
+    
+    @Override
+    public String toString() {
+        return "Pengaturan{" +
+                "id=" + id +
+                ", namaSekolah='" + namaSekolah + '\'' +
+                ", alamatSekolah='" + alamatSekolah + '\'' +
+                ", tahunAjaranAktif='" + tahunAjaranAktif + '\'' +
+                ", kepalaSekolah='" + kepalaSekolah + '\'' +
+                '}';
+    }
 }
